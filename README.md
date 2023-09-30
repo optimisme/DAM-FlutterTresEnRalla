542149de8c750ff9fab253c2ebb5b01883d5a31a
optimisme <optimisme@gmail.com>
2023-09-30 11:22:32 +0200

    First version

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# DAM-FlutterCupertinoBase
+# Base Flutter Project for Cuppertino look and feel
+
+This project is a starting point for a Flutter application with a Cupertino look and feel.
+
+## Getting Started
+
+Get dependencies and rename the project with:
+
+``` bash
+cd cupertino_base
+flutter pub get
+flutter create --project-name <project_name> .
+cd ..
+cd mv cupertino_base <project_name>
+```
---
+++ cupertino_base/lib/app.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/cupertino.dart';
+import 'layout_desktop.dart';
+import 'layout_mobile.dart';
+
+// Main application widget
+class App extends StatefulWidget {
+    const App({Key? key}) : super(key: key);
+
+    @override
+    AppState createState() => AppState();
+}
+
+// Main application state
+class AppState extends State<App> {
+
+    // Define the layout to use depending on the screen width
+    Widget _setLayout(BuildContext context) {
+        // Set different layouts depending on the screen width
+        double width = MediaQuery.of(context).size.width;
+        if (width >= 600) {
+            return const LayoutDesktop(title: "App Desktop Title");
+        } else {
+            return const LayoutMobile(title: "App Mobile Title");
+        }
+    }
+
+  // Definir el contingut del widget 'App'
+  @override
+  Widget build(BuildContext context) {
+
+    // Farem servir la base 'Material'
+    return CupertinoApp(
+        debugShowCheckedModeBanner: false,
+        theme: CupertinoThemeData(brightness: Brightness.light),
+        home: _setLayout(context),
+    );
+  }
+}
